bb364d432cf11ee898fc534791b781d63d668cbe
Jesse Tomchak <jtomchak@gmail.com>
2018-04-04 13:58:31 -0700

    MoviePosterList in card with clearfix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { Grid, Navbar, Jumbotron, Button } from "react-bootstrap";
+import MovieCards from "./MovieCards";
 const MOVIE_URL =
   "https://api.themoviedb.org/3/search/movie?api_key=2434d246ec60c162a86db597467ef4ed&language=en-US&query=hiphop&include_adult=false&sort_by=created_at.asc&page=1";
+
 class App extends Component {
   state = {
     movies: []
@@ -11,19 +13,19 @@ class App extends Component {
       .then(response => response.json())
       .then(payload =>
         this.setState({
-          movies: payload.results
+          movies: payload.results.filter(m => m.poster_path)
         })
       )
       .catch(err => console.log(err));
   }
   render() {
     return (
-      <div>
+      <div style={{ backgroundColor: "lightgray" }}>
         <Navbar inverse fixedTop>
           <Grid>
             <Navbar.Header>
               <Navbar.Brand>
-                <a href="/">React App</a>
+                <a href="/">Movie Town App</a>
               </Navbar.Brand>
               <Navbar.Toggle />
             </Navbar.Header>
@@ -31,19 +33,10 @@ class App extends Component {
         </Navbar>
         <Jumbotron>
           <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank"
-              >
-                View React Bootstrap Docs
-              </Button>
-            </p>
+            <h1>Hip Hop Movies</h1>
           </Grid>
         </Jumbotron>
+        <MovieCards movies={this.state.movies} />
       </div>
     );
   }
---
+++ src/MoviePosterList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Grid, Row, Col, Thumbnail, Button, Clearfix } from "react-bootstrap";
+
+const MoviePosterList = ({ movies }) => {
+  //const MovieCardItem = props => {
+  // let movies = props.movies;
+  return (
+    <Row>
+      {movies.map((movie, idx) => (
+        <div key={movie.id}>
+          <Col sm={6} md={4}>
+            <Thumbnail src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="242x200">
+              <h3>{movie.title}</h3>
+              <p>{movie.overview}</p>
+              <p>
+                <Button bsStyle="primary">Details</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+          {(idx + 1) % 2 === 0 && <Clearfix visibleSmBlock />}
+          {(idx + 1) % 3 === 0 && <Clearfix visibleMdBlock visibleLgBlock />}
+        </div>
+      ))}
+    </Row>
+  );
+};
+
+export default MoviePosterList;
